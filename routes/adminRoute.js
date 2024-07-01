f33ef981b2ffc7ac822d747e8989cd7103f76178
Hayzed-codes <raheemhorlah@gmail.com>
2024-07-01 14:42:44 +0100

    Controllers

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const { adminRegister, adminLogin } = require("../Controllers/UserController");
 
-router.post("/register", userRegister);
-router.post("/login", userLogin);
+router.post("/register", adminRegister);
+router.post("/login", adminLogin);
