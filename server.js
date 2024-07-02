d6b7ba01e2ac24d64ca1606747e34072d4bd89cb
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-07-02 14:27:28 +0100

    Update

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const connectDB = require("./config/DBConnet")
 const errorHandler = require("./middleware/errorMiddleware");
-const adminRoute = require("./routes/adminRoute_nm");
-const userRoute = require("./routes/userRoute_nm");
+const adminRoute = require("./routes/adminRoute");
+const userRoute = require("./routes/userRoute");
 
 
 const PORT = process.env.PORT || 3500;
