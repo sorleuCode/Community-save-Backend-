9caf7259a8b9de5df3fbf39761698fc0c1f041b0
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-07-02 13:45:10 +0100

    update

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    fullname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     paystackCustomerId: { type: String, required: true }, // Paystack customer ID
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -9,10 +9,5 @@ router.post("/login", adminLogin)
 
 module.exports = router
 
-const { adminRegister, adminLogin } = require("../Controllers/AdminController");
 
-router.post("/register", adminRegister);
-router.post("/login", adminLogin);
-
-module.exports = router;
 
