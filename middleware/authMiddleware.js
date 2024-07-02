26267ca453f5ab6e1f5d54655ca70c60dda82188
muritalazainab <muritala21@icloud.com>
2024-07-02 11:41:49 +0100

    authHandle

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
     const token =  req.headers.authorization.split(" ")[1];
 
     if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
@@ -20,9 +20,6 @@ const isAdmin = (req, res, next) => {
 };
 
 module.exports = { verifyToken, isAdmin };
-
-
-
 
 
  
